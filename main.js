3eba00093930bcfcb6dda2926c229acfbe27c5ed
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-10 10:25:25 +0100

    Risolto

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ let prezzoOver = prezzoBase - (prezzoBase/100 * 40);
 if ( etaUtente < 18 ) {
     //istruzioni se condizione è vera
     console.log(prezzoUnder.toFixed(2))
-    document.getElementById("contenitore_prezzo").innerHTML = <h2>Il prezzo per il suo biglietto è  ${prezzoUnder}</h2>;
+    document.getElementById("contenitore_prezzo").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoUnder}€</h2>`;
   } else if ( etaUtente > 65) {
     //istruzioni se condizione 2 è vera
     console.log(prezzoOver.toFixed(2))
-    document.getElementById("contenitore_prezzo").innerHTML = <h2>Il prezzo per il suo biglietto è  ${prezzoOver}</h2>;
+    document.getElementById("contenitore_prezzo").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoOver}€</h2>`;
   } else {
     //istruzioni nessuna condizione precedente è vera
     console.log(prezzoBase.toFixed(2))
-    document.getElementById("contenitore_prezzo").innerHTML = <h2>Il prezzo per il suo biglietto è  ${prezzoBase}</h2>;
+    document.getElementById("contenitore_prezzo").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoBase}€</h2>`;
   }
