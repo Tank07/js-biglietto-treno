6e2c2956a1aa5dace1dea6855297a09c9bc4ba76
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-10 01:19:41 +0100

    Struttura finita, manca estetica

--- main.js
+++ main.js
@@ -8,27 +8,35 @@ let prezzoBase = chilometriUtente * 0.21;
 
 console.log(prezzoBase);
 
-let prezzoUnder = prezzoBase - (prezzoBase/100 * 20);
+let prezzoUnder = prezzoBase - (prezzoBase/100 * 20); 
 
 let prezzoOver = prezzoBase - (prezzoBase/100 * 40);
 
 if ( etaUtente < 18 ) {
     //istruzioni se condizione è vera
-    console.log(prezzoUnder.toFixed(2));
+    console.log(prezzoUnder.toFixed(2))
+    document.getElementById("contenitore_prezzo").innerHTML = prezzoUnder;
   } else if ( etaUtente > 65) {
     //istruzioni se condizione 2 è vera
-    console.log(prezzoOver.toFixed(2));
+    console.log(prezzoOver.toFixed(2))
+    document.getElementById("contenitore_prezzo").innerHTML = prezzoOver;
   } else {
     //istruzioni nessuna condizione precedente è vera
-    console.log(prezzoBase.toFixed(2));
+    console.log(prezzoBase.toFixed(2))
+    document.getElementById("contenitore_prezzo").innerHTML = prezzoBase;
   }
 
+//   if ( etaUtente < 18 ) {
+//     //istruzioni se condizione è vera
+//     document.getElementById("contenitore_prezzo").innerHTML = prezzoUnder;
+//   } else if ( etaUtente > 65) {
+//     //istruzioni se condizione 2 è vera
+//     document.getElementById("contenitore_prezzo").innerHTML = prezzoOver;
+//   } else {
+//     //istruzioni nessuna condizione precedente è vera
+//     document.getElementById("contenitore_prezzo").innerHTML = prezzoBase;
+//   }
 
-
-// let password = ` La tua password indecifrabile è: ${nomeUtente}${cognomeUtente}${coloreUtente}${numeroUtente}`;
-
-// console.log(password);
-
-// document.getElementById("contenitore_password").innerHTML = password;
+// document.getElementById("contenitore_prezzo").innerHTML = prezzoUnder;
 
 // document.getElementById("contenitore_password").style.color = "magenta";
