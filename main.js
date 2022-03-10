ebf9ff0912fcf9a07030516a5b511d2a9c667e4e
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-10 10:22:02 +0100

    l'ho rotto....

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let chilometriUtente = prompt("Quanti chilometri dovrà percorrere ?");
+let chilometriUtente = Number(prompt ("Quanti chilometri dovrà percorrere ?"));
 
-let etaUtente = prompt("Quanti anni ha ?");
+let etaUtente = Number(prompt ("Quanti anni ha ?"));
 
-console.log(chilometriUtente +  etaUtente);
+console.log(chilometriUtente + etaUtente);
 
 let prezzoBase = chilometriUtente * 0.21;
 
@@ -15,28 +15,13 @@ let prezzoOver = prezzoBase - (prezzoBase/100 * 40);
 if ( etaUtente < 18 ) {
     //istruzioni se condizione è vera
     console.log(prezzoUnder.toFixed(2))
-    document.getElementById("contenitore_prezzo").innerHTML = prezzoUnder;
+    document.getElementById("contenitore_prezzo").innerHTML = <h2>Il prezzo per il suo biglietto è  ${prezzoUnder}</h2>;
   } else if ( etaUtente > 65) {
     //istruzioni se condizione 2 è vera
     console.log(prezzoOver.toFixed(2))
-    document.getElementById("contenitore_prezzo").innerHTML = prezzoOver;
+    document.getElementById("contenitore_prezzo").innerHTML = <h2>Il prezzo per il suo biglietto è  ${prezzoOver}</h2>;
   } else {
     //istruzioni nessuna condizione precedente è vera
     console.log(prezzoBase.toFixed(2))
-    document.getElementById("contenitore_prezzo").innerHTML = prezzoBase;
+    document.getElementById("contenitore_prezzo").innerHTML = <h2>Il prezzo per il suo biglietto è  ${prezzoBase}</h2>;
   }
-
-//   if ( etaUtente < 18 ) {
-//     //istruzioni se condizione è vera
-//     document.getElementById("contenitore_prezzo").innerHTML = prezzoUnder;
-//   } else if ( etaUtente > 65) {
-//     //istruzioni se condizione 2 è vera
-//     document.getElementById("contenitore_prezzo").innerHTML = prezzoOver;
-//   } else {
-//     //istruzioni nessuna condizione precedente è vera
-//     document.getElementById("contenitore_prezzo").innerHTML = prezzoBase;
-//   }
-
-// document.getElementById("contenitore_prezzo").innerHTML = prezzoUnder;
-
-// document.getElementById("contenitore_password").style.color = "magenta";
